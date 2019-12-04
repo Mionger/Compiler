a16796a566c29d0160bdc20851fb55a18ecc7568
Park Mion-ger <33119492+Mionger@users.noreply.github.com>
2019-12-04 16:30:10 +0800

    Create word.py

---
+++ Parser/word.py
@@ -0,0 +1,7 @@
+class Word(object):
+    def __init__(self):
+        self.value = ""
+        self.type = -1
+        self.lineCount = -1
+        self.startCharIndex = -1
+        self.endCharIndex = -1
